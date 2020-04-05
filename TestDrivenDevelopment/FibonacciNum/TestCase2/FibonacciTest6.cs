4774db561ee83e89d967c9031ca95a636d315068
Jackie <JGtheophila19@gmail.com>
2020-04-05 15:36:54 -0400

    test case passed

---
+++ TestDrivenDevelopment/FibonacciNum/TestCase2/FibonacciTest6.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collection.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TestDrivenDevelopment
+{
+    /* Sequence of Fibonacci Number
+        1, 1, 2, 3, 5, 8, 13, 21, 34 or
+        0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+    */
+    
+    [TextFixture]
+    public class FibonacciTests
+    {
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(1, 2)]
+        [TestCase(2, 3)]
+        public void TestFibonacci(int expected, int index)
+        {
+            Assert.AreEqual(expected, GetFibonacci(index));   
+        }
+        
+        private int GetFibonacci(int index)
+        {
+            if(index == 0) return 0;
+            if(index <= 2) return 1;
+            return GetFibonacci(index - 1) + GetFibonacci(index - 2);
+        }
+    }
+}
