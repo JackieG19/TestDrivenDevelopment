84a6e5d5911f7c0c3433e80771012d075857a581
Jackie <JGtheophila19@gmail.com>
2020-04-07 17:43:05 -0400

    parse failed

---
+++ TestDrivenDevelopment/RomanNumerals/TestCase6/RomanNumeralsTest1.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+
+namespace TDD
+{
+	[TestFixture]
+	public class RomanNumeralsTests
+	{
+		[Test(1, "I")]
+		[Test(5, "V")]
+		[Test(10, "X")]
+		[Test(2, "II")]
+		public void Parse(int expected, string roman)
+		{
+			Assert.AreEqual(expected, Roman.Parse(roman));
+		}
+	}
+	
+	public class Roman
+	{
+		private static Dictionary<char, int> map = new Dictionary<char, int>()
+		{
+			{'I', 1},
+			{'V', 5},
+			{'X', 10},
+			{'L', 50},
+			{'C', 100},
+			{'D', 500},
+			{'M', 1000}
+		};
+		
+		public static in Parse(string roman)
+		{
+			return map[roman[0]];
+		}
+	}
+}
