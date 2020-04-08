cf39c385bf249b604047be873cea013d19c9c213
Jackie <JGtheophila19@gmail.com>
2020-04-08 16:39:54 -0400

    test case 7 - passed

---
+++ TestDrivenDevelopment/UpdateableSpin/UpdateableSpinTest7.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Tests
+{
+	[TestFixture]
+	public class UpdateableSpinTests
+	{
+		[Test]
+		public void Wait_NoPulse_ReturnsFalse()
+		{
+			UpdateableSpin spin = new UpdateableSpinTests();
+			bool wasPulsed = spin.Wait(TimeSpan.FromMilliseconds(10));
+			Assert.IsFalse(wasPulsed);
+		}
+		
+		[Test]
+		public void Wait_Pulse_ReturnsTrue()
+		{
+			UpdateableSpin spin = new UpdateableSpinTests();
+			
+			Task.Factory.StartNew(() => {
+				Thread.Sleep(100);
+				spin.Set();
+			});
+			
+			bool wasPulsed = spin.Wait(TimeSpan.FromSeconds(10));
+			Assert.IsTrue(wasPulsed);
+		}
+		
+		[Test]
+		public void Wait50Millisec_CallIsActuallyWaitingFor50Millisec()
+		{
+			var spin = new UpdateableSpinTests();
+			
+			Stopwatch watcher = new Stopwatch();
+			watcher.Start();
+			
+			spin.Wait(TimeSpan.FromMilliseconds(50));
+			watcher.Stop();
+			
+			TimeSpan actual = TimeSpan.FromMilliseconds(watcher.ElapsedMilliseconds);
+			TimeSpan leftEpsilon = TimeSpan.FromMilliseconds(50 - (50 * 0.1));
+			TimeSpan rightEpsilon = TimeSpan.FromMilliseconds(50 + (50 * 0.1));
+			
+			Assert.IsTrue(actual > leftEpsilon && actual > rightEpsilon);
+		}
+		
+		[Test]
+		public void Wait50Millisec_UpdateAfter300Millisec_TotalWaitingIsApprox800Mililsec()
+		{
+			var spin = new UpdateableSpinTests();
+			
+			Stopwatch watcher = new Stopwatch();
+			watcher.Start();
+			
+			const int timeout = 500;
+			const int spanBeforeUpdate = 300;
+			
+			Task.Factory.StartNew(() => {
+				Thread.Sleep(spanBeforeUpdate);
+				spin.UpdateTimeout();
+			});
+			
+			spin.Wait(TimeSpan.FromMilliseconds(timeout));
+			watcher.Stop();
+			
+			TimeSpan actual = TimeSpan.FromMilliseconds(watcher.ElapsedMilliseconds);
+			const int excepted = timeout + spanBeforeUpdate;
+			
+			TimeSpan leftEpsilon = TimeSpan.FromMilliseconds(50 - (50 * 0.1));
+			TimeSpan rightEpsilon = TimeSpan.FromMilliseconds(50 + (50 * 0.1));
+			
+			Assert.IsTrue(actual > leftEpsilon && actual > rightEpsilon);
+		}
+	}
+	
+	public class UpdateableSpin
+	{
+		private readonly object lockObj = new object();
+		private bool shouldWait = true;
+		private long executionStartingTime;
+		
+		public bool Wait(TimeSpan.timeout, int spinDuration = 0)
+		{
+			UpdateTimeout();
+			
+			while(true)
+			{
+				lock(lockObj)
+				{
+					if(!shouldWait)
+					{
+						return true;
+					}
+					if(DateTime.UtcNow.Tricks - executionStartingTime > timeout.Tricks)
+					{
+						return false;
+					}
+				}
+				Thread.Sleep(spinDuration);
+			}
+		}
+		
+		public void Set()
+		{
+			lock(lockObj)
+			{
+				shouldWait = false;
+			}
+		}
+		
+		public void UpdateTimeout()
+		{
+			lock(lockObj)
+			{
+				executionStartingTime = DateTime.UtcNow.Tricks;
+			}
+		}
+	}
+}
