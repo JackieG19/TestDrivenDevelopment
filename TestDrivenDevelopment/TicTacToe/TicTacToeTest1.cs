49b98b2e3656480b2f46bd2ae82f6c354a4fe830
Jackie <JGtheophila19@gmail.com>
2020-04-08 19:00:50 -0400

    test case 1 - passed

---
+++ TestDrivenDevelopment/TicTacToe/TicTacToeTest1.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TDD
+{
+	[TestFixture]
+	public class TicTacToeTests
+	{
+		[Tests]
+		public void CreateGame_ZeroMoves()
+		{
+			Game game = new Game();
+			Assert.AreEqual(0, game.MovesCounter);
+		}
+	
+	public class Game
+	{
+		public int MovesCounter { get; private set; }
+	}
+}
